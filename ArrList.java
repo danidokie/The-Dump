4a7cc037493ff095011d3a3d081a11b86b4367ce
Dania Cristobal <139860614+danidokie@users.noreply.github.com>
2024-09-23 10:24:22 -0400

    Add files via upload

--- ArrList.java
+++ ArrList.java
@@ -45,12 +45,12 @@ public class ArrList<E> extends ArrListAdapter<E>
    /* TODO: implement void add(int index, E element) method
             insert element in the ArrList at the given index, moving existing 
             elements to the right 
-            Throws IndexOutofBoundsException if the index is out of range.*/
+   Throws IndexOutofBoundsException if the index is out of range.*/
    public void add(int index, E element)
    {
-      if(index < 0 || index > size)
+      if(index < 0 || index >= size)
       {
-         throw new IndexOutofBoundsException();
+         throw new IndexOutOfBoundsException();
       }
       
       add(element);
@@ -62,28 +62,78 @@ public class ArrList<E> extends ArrListAdapter<E>
    }
             
    // TODO: implement int size() method returing the size of the ArrList.
-   
+   public int size()
+   {
+      return size;
+   }
+    
    /* TODO: implement E get(int index) method, returns element at index 
-            
-            Throws IndexOutofBoundsException if the index is out of range.*/
+   Throws IndexOutofBoundsException if the index is out of range.*/
+   public E get(int index)
+   {
+      if(index < 0 || index >= size)
+      {
+         throw new IndexOutOfBoundsException();
+      }
+      return data[index];
+   }
    
    /* TODO: implement E set(int index, E element) method, replace the existing
             value at index with element, return the old value. 
-            
-            Throws IndexOutofBoundsException if the index is out of range.*/
+   Throws IndexOutofBoundsException if the index is out of range.*/
+   public E set(int index, E element)
+   {
+      E oldElement = get(index);
+      data[index] = element;
+      return oldElement;
+   }
    
    /* TODO: implement int indexOf(Object o) method, return the index of the 
             first occurrence of o in the ArrList, or -1 if the ArrList does
             not contain the element. */
+   public int indexOf(Object o)
+   {
+      for(int i = 0; i < data.length; i++)
+      {
+         if(o.equals(data[i]));
+         {
+            return i;
+         }
+      }
+      return -1;
+   }
    
    /* TODO: implement boolean contains(Object o) method, return true of the list
             contains the element. */
+   public boolean contains(Object o)
+   {
+      return indexOf(o) != -1;
+   }
    
-   /* TODO: implement E remove(int index) method, remove the element at the 
+   /* TODO: implement E remove(int index) method, remove and return the element at the 
             the specified index. 
-            
-            Throws IndexOutofBoundsException if the index is out of range.*/
+   Throws IndexOutofBoundsException if the index is out of range.*/
+   public E remove(int index)
+   {
+      E oldElement = get(index);
+      for(int i = index; i < size -1; i++)
+      {
+         data[i] = data[i + 1];
+      }
+      size--;
+      return oldElement;
+   }
    
    /* TODO: implement boolean remove(Object o) method, remove the first occurrence of 
             the specified element. */
+   public boolean remove(Object o)
+   {
+      int index = indexOf(o);
+      if(index == -1)
+      {
+         return false;
+      }
+      remove(index);
+      return true;
+   }
 }
--- TestArrList.java
+++ TestArrList.java
@@ -9,6 +9,27 @@ public class TestArrList
       myList.add("c");
       myList.add("d");
       myList.add("e");
-      myList.add(2, "g");
+      myList.add("f");
+      //myList.add(85, "g");
+      System.out.println(myList.size());
+      System.out.println(myList.get(2));
+      System.out.println(myList.get(0));
+      System.out.println(myList.get(4));
+      //System.out.println(myList.get(5));
+      //System.out.println(myList.get(-1));
+      System.out.println(myList.set(0, "g"));
+      System.out.println(myList.set(4, "h"));
+      //System.out.println(myList.set(-1, "w"));
+      //System.out.println(myList.set(5, "x"));
+      System.out.println(myList.indexOf("g"));
+      System.out.println(myList.indexOf("h"));
+      System.out.println(myList.indexOf("i"));
+      System.out.println(myList.indexOf("d"));
+      System.out.println(myList.contains("e"));
+      System.out.println(myList.contains("a"));
+      System.out.println(myList.remove(2));
+      System.out.println(myList.add("z"));
+      System.out.println(myList.remove("c"));
+      System.out.println(myList.remove("a"));
    }
 }
