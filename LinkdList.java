1dacdd3a7b4237c3520cac64a0b345d365cf2963
Dania Cristobal <139860614+danidokie@users.noreply.github.com>
2024-09-25 10:45:36 -0400

    Add files via upload

--- LinkdList.java
+++ LinkdList.java
@@ -54,27 +54,112 @@ public class LinkdList<E> extends LinkdListAdapter<E>
    /* TODO: implement void add(int index, E element) method
             insert element in the list at the given index, moving existing 
             elements to the right */
+   public void add(int index, E element)
+   {
+      Node newNode = new Node(element);
+      if(index == 0)
+      {
+         newNode.next = head;
+         head = newNode;
+      }
+      else
+      {
+         Node previousNode = getNode(index - 1);
+         newNode.next = previousNode.next;
+         previousNode.next = newNode;
+      }
+      size++;
+   }
    
+   private Node getNode(int index)
+   {
+      if(index < 0 || index >= size)
+      {
+         throw new IndexOutOfBoundsException();
+      }
+      
+      Node node = head; 
+      for(int i = 0; i < index; i++)
+      {
+         node = node.next;
+      }
+      return node;
+   }
             
    // TODO: implement int size() method returing the size of the list.
-   
-   
+   public int size()
+   {
+      return size;
+   }
+
    // TODO: implement E get(int index) method, returns element at index.
-   
+   public E get(int index)
+   {
+      return getNode(index).element;
+   }
    
    /* TODO: implement E set(int index, E element) method, replace the existing
             value at index with element, return the old value */
+   public E set(int index, E element)
+   {
+      Node node = getNode(index);
+      E oldValue = node.element;
+      node.element = element;
+      return oldValue;
+   }
    
    /* TODO: implement int indexOf(Object o) method, return the index of the 
             first occurrence of o in the list, or -1 if the list does
             not contain the element. */
+   public int indexOf(Object o)
+   {
+      Node node = head;
+      for(int i = 0; i < size; i++)
+      {
+         if(node.element.equals(o))
+         {
+            return i;
+         }
+         node = node.next;
+      }
+      return -1;
+   }
    
    /* TODO: implement boolean contains(Object o) method, return true of the list
             contains the element. */
+   public boolean contains(Object o)
+   {
+      return indexOf(o) != -1;
+   }
    
    /* TODO: implement E remove(int index) method, remove the element at the 
             the specified index. */
+   public E remove(int index)
+   {
+      E oldValue = get(index);
+      if(index == 0)
+      {
+         head = head.next;
+      }
+      else
+      {
+         Node node = getNode(index -1);
+         node.next = node.next.next;
+      }
+      size --;
+      return oldValue;
+   }
    
    /* TODO: implement boolean remove(Object o) method, remove the first occurrence of 
             the specified element, */
+   public boolean remove(Object o)
+   {
+      int index = indexOf(o);
+      if(index == -1)
+      {
+         return false;
+      }
+      remove(index);
+      return true;
+   }
 }
--- TestLinkdList.java
+++ TestLinkdList.java
@@ -9,8 +9,32 @@ public class TestLinkdList
       words.add("b");
       words.add("c");
       words.add("d");
+      words.add(2, "e");
+      words.add(4, "f");
+      words.add(0, "g");
+      System.out.println(words.size());
+      System.out.println(words.get(0));
+      System.out.println(words.get(6));
+      System.out.println(words.get(4));
+      words.set(0, "h");
+      words.set(6, "i");
+      words.set(3, "j");
+      System.out.println(words.indexOf("a"));
+      System.out.println(words.indexOf("c"));
+      System.out.println(words.indexOf("h"));
+      System.out.println(words.indexOf("i"));
+      System.out.println(words.indexOf("d"));
+      System.out.println(words.contains("d"));
+      System.out.println(words.contains("i"));
+      words.remove(0);
+      words.remove(5);
+      words.remove(1);
+      words.remove("a");
+      words.remove("j");
+      words.remove("f");
+      words.remove("a");
       
-      List<Integer> numbers = new LinkdList<>();
-      numbers.add(45);
+      //List<Integer> numbers = new LinkdList<>();
+      //numbers.add(45);
    }
 }
